aeb5e17b006ac7fa4e28dbf0c76a5276ce6a1933
Franco Covarrubias-Reyes <fcovarrubiasreyes@yahoo.com>
2018-09-01 17:28:17 -0500

    Added code for admin page

---
+++ src/views/adminLoggedIn.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="secure">
+        <h1>"Vigenere wept, for there were no more worlds to conquer"</h1>
+        <p>
+            flag = csg{}
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'adminLoggedIn',
+        data() {
+            return {};
+        }
+    }
+</script>
+
+<style scoped>
+    #secure {
+        background-color: #FFFFFF;
+        border: 1px solid #CCCCCC;
+        padding: 20px;
+        margin-top: 10px;
+    }
+</style>
