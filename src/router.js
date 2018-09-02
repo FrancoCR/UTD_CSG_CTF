6b793386668efbbd6147b9f688e6ac689b1ca2a0
Franco Covarrubias-Reyes <fcovarrubiasreyes@yahoo.com>
2018-09-02 11:34:42 -0500

    Fixed router.js
    Im stupid and wasn't paying attention

--- src/router.js
+++ src/router.js
@@ -1,29 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
-Vue.use(Router)
-
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
-})
-
-import Vue from 'vue'
-import Router from 'vue-router'
 import LoginComponent from "./views/login.vue"
 import SecureComponent from "./views/adminLoggedIn.vue"
 
@@ -51,6 +27,19 @@ export default new Router({
             path: "/adminLoggedIn",
             name: "adminLoggedIn",
             component: SecureComponent
+        },
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+           path: '/about',
+           name: 'about',
+           // route level code-splitting
+           // this generates a separate chunk (about.[hash].js) for this route
+           // which is lazy-loaded when the route is visited.
+           component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
         }
     ]
 })
