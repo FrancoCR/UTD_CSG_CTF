e13b9ef2761f38d310199a85220f64acde814655
Franco Covarrubias-Reyes <fcovarrubiasreyes@yahoo.com>
2018-09-01 19:01:48 -0500

    Added the gobuster page

---
+++ src/views/ghostbusters.vue
@@ -0,0 +1,9 @@
+<template>
+    <div id="secure">
+        <h1>GhostBusters!!! Well gobuster but obviously you figured that out... Or you did something else. Goodjob.</h1>
+        <p>
+            User: admin
+            Password: W3lLTh@tw@$fuCk1ng3asY
+        </p>
+    </div>
+</template>
